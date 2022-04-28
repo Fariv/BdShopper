78d4924502cff6cd40acd424c49e1a02a976a263
Ashraful Ferdous <a.f.fariv@gmail.com>
2022-04-29 02:13:17 +0600

    responsiveness to register, login, product, product list and cart pages

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,11 +7,13 @@ import { Footer } from '../components/Footer';
 import { Navbar } from '../components/Navbar';
 import { Newsletter } from '../components/Newsletter';
 import Shirt01 from '../assets/shirt_01.png';
+import { mobile } from '../responsive';
 
 const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+  ${mobile({ padding: "10px", flexDirection:"column" })}
 `;
 const ImageContainer = styled.div`
   flex: 1;
@@ -19,11 +21,13 @@ const ImageContainer = styled.div`
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0 50px;
+  ${mobile({ padding: "10px" })}
 `;
 const Image = styled.img`
   width: 100%;
   height: 90vh;
   object-fit: cover;
+  ${mobile({ height: "40vh" })}
 `;
 const Title = styled.h1`
   font-weight: 300;
@@ -41,6 +45,7 @@ const FilterContainer = styled.div`
   width: 50%;
   display: flex;
   justify-content: space-between;
+  ${mobile({ width: "100%" })}
 `;
 const Filter = styled.div`
   display: flex;
@@ -70,6 +75,7 @@ const AddContainer = styled.div`
   align-items: center;
   width: 50%;
   justify-content: space-between;
+  ${mobile({ width: "100%" })}
 `;
 const AmountContainer = styled.div`
   display: flex;
